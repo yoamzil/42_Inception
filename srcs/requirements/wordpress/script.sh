918729d0b813215cd1b193459a3bffc82ffd6666
Youness Amzil <amzilyouness@gmail.com>
2024-08-18 11:52:51 -0400

    Added a second user.

--- srcs/requirements/wordpress/script.sh
+++ srcs/requirements/wordpress/script.sh
@@ -7,6 +7,8 @@ curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.pha
 chmod +x wp-cli.phar
 ./wp-cli.phar core download --allow-root
 ./wp-cli.phar config create --dbname=${MYSQL_DATABASE} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASSWORD} --dbhost=mariadb  --skip-check --allow-root
-./wp-cli.phar core install --url=${WP_URL} --title=inception --admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=admin@${WP_ADMIN_EMAIL} --allow-root
+
+./wp-cli.phar core install --url=${WP_URL} --title=inception --admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL} --allow-root
+./wp-cli.phar user create ${WP_USER} ${WP_USER_EMAIL} --role=author --user_pass=${WP_USER_PASSWORD} --allow-root
 
 exec $@
